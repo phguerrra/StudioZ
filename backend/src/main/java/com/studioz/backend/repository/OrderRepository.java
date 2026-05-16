ff52cb1dbec3b1a1ae378e6f8d401aa135126dde
Gabriel Fernandes <gabrielpaesfernandes0@gmail.com>
2026-05-16 12:13:14 -0300

    feat: adiciona repositorios principais do sistema

---
+++ backend/src/main/java/com/studioz/backend/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.studioz.backend.repository;
+
+import com.studioz.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<User, Long> {
+
+}
