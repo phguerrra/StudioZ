37058576e36827df9926d63ec92891873cd6afcc
Gabriel Fernandes <gabrielpaesfernandes0@gmail.com>
2026-05-16 12:39:12 -0300

    feat: configura estrutura inicial do backend spring boot

--- backend/src/main/java/com/studioz/backend/model/Order.java
+++ backend/src/main/java/com/studioz/backend/model/Order.java
@@ -1,10 +1,18 @@
 package com.studioz.backend.model;
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 
+@Entity
+@Table(name = "orders")
 public class Order {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
+        @ManyToOne
         private User user;
+        @ManyToOne
         private Product product;
         private int quantity;
         private double totalPrice;
--- backend/src/main/java/com/studioz/backend/model/Product.java
+++ backend/src/main/java/com/studioz/backend/model/Product.java
@@ -1,16 +1,21 @@
 package com.studioz.backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.awt.*;
 
 @Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String description;
     private double basePrice;
-    private Image image;
+    private String imagem;
 
 
 
